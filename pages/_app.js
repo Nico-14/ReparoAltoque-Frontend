c5c25c302e0b8a688950fa604e014f14441f3665
Nicolas Ledesma <mateo.14.ledesma@gmail.com>
2020-08-14 18:34:03 -0300

    Terminado diseño registro, con validación

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/scss/argon-design-system.scss'
 import '../styles/font-awesome.css'
+import '../styles/argon-design-system.min.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
