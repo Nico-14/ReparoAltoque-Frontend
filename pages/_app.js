3bb92b7d464fd5771146dfbc93a2af5aaea98951
Nicolas Ledesma <mateo.14.ledesma@gmail.com>
2020-08-14 15:15:49 -0300

    Quité tailwind y agregué reacstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/argon-design-system.min.css'
+import '../styles/font-awesome.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
