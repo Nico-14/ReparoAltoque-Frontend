23e84495ffca0d9bf4e51d42a63bab2c3b690fc3
Gonzalo Corvalan <mr.corvy@gmail.com>
2020-08-17 18:04:56 -0300

    aca terminé casi todo lo que tenia planeado para hoy

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,142 +1,35 @@
 import Layout from "../components/Layout";
-// import Input from "../components/Input";
-// import Button from "../components/Button";
 
 import SectionWelcome from "../components/Sections/SectionWelcome";
 import SectionBuscar from "../components/Sections/SectionBuscar";
 import SectionIniciar from "../components/Sections/SectionIniciar";
-
-import { Col, Card, CardImg, CardBody, CardText, CardTitle } from "reactstrap";
+import SectionHorizontalScroll from "../components/Sections/SectionHorizontalScroll";
 
 export default function Home() {
+    let arrayDePrueba = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    //mismo que en el form registro, pero es una petición get
+    //la url base + /Posts/showWelcome
+    //acá se devuelven 2 arrays, uno de trabajo a distancia y otro de presenciales
+    //actualmente hay como 4 publicaciones en la base de datos, asi que el llamado va adevolver solo 1 o 2 resultados
+
+    //y otra cosa, para listar la lista (valga la redundancia) de rubros de trabajo es otra petición get
+    //esta vez a la url base + /LineWorks/show
+
     return (
         <Layout>
             <div className="main" style={{ minHeight: "100vh" }}>
                 <SectionWelcome />
                 <SectionBuscar />
-                <section className="section bg-secondary">
-                    <Col md="12">
-                        <h3 className="title text-center display-3 mb-5">
-                            Algunos trabajos a distancia Recomendados
-                        </h3>
-                    </Col>
-                    <Col
-                        md="12"
-                        className="d-flex justify-content-center"
-                        style={{
-                            overflowX: "scroll",
-                        }}
-                    >
-                        <Col
-                            md="4"
-                            className="d-inline"
-                            // style={{ minWidth: "15rem" }}
-                        >
-                            <Card>
-                                <CardImg
-                                    alt="..."
-                                    src="https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/alejandro-escamilla.jpg"
-                                    top
-                                ></CardImg>
-                                <CardBody>
-                                    <CardTitle>Card title</CardTitle>
-
-                                    <CardText>
-                                        <small className="text-muted">
-                                            Nombre del profesional
-                                        </small>
-                                        <br />
-                                        <br />
-                                        <small className="text-muted">
-                                            las estrellas de puntuación
-                                        </small>
-                                    </CardText>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col
-                            md="4"
-                            className="d-inline"
-                            // style={{ minWidth: "15rem" }}
-                        >
-                            <Card>
-                                <CardImg
-                                    alt="..."
-                                    src="https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/alejandro-escamilla.jpg"
-                                    top
-                                ></CardImg>
-                                <CardBody>
-                                    <CardTitle>Card title</CardTitle>
-
-                                    <CardText>
-                                        <small className="text-muted">
-                                            Nombre del profesional
-                                        </small>
-                                        <br />
-                                        <br />
-                                        <small className="text-muted">
-                                            las estrellas de puntuación
-                                        </small>
-                                    </CardText>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col
-                            md="4"
-                            className="d-inline"
-                            // style={{ minWidth: "15rem" }}
-                        >
-                            <Card>
-                                <CardImg
-                                    alt="..."
-                                    src="https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/alejandro-escamilla.jpg"
-                                    top
-                                ></CardImg>
-                                <CardBody>
-                                    <CardTitle>Card title</CardTitle>
-
-                                    <CardText>
-                                        <small className="text-muted">
-                                            Nombre del profesional
-                                        </small>
-                                        <br />
-                                        <br />
-                                        <small className="text-muted">
-                                            las estrellas de puntuación
-                                        </small>
-                                    </CardText>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col
-                            md="4"
-                            className="d-inline"
-                            // style={{ minWidth: "15rem" }}
-                        >
-                            <Card>
-                                <CardImg
-                                    alt="..."
-                                    src="https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/alejandro-escamilla.jpg"
-                                    top
-                                ></CardImg>
-                                <CardBody>
-                                    <CardTitle>Card title</CardTitle>
-
-                                    <CardText>
-                                        <small className="text-muted">
-                                            Nombre del profesional
-                                        </small>
-                                        <br />
-                                        <br />
-                                        <small className="text-muted">
-                                            las estrellas de puntuación
-                                        </small>
-                                    </CardText>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Col>
-                </section>
+                <SectionHorizontalScroll
+                    titulo="Trabajos a distancia que recomendamos"
+                    arrayPublicaciones={arrayDePrueba}
+                    bgColor="bg-secondary"
+                />
+                <SectionHorizontalScroll
+                    titulo="Trabajos a domicilio que recomendamos"
+                    arrayPublicaciones={arrayDePrueba}
+                    bgColor="bg-white"
+                />
                 <SectionIniciar />
             </div>
         </Layout>
