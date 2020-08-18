3a14739293ce238de8510db94f3db364529713c7
Gonzalo Corvalan <mr.corvy@gmail.com>
2020-08-18 18:08:49 -0300

    hice la view de elejir rubro, falta añadirle la lógica para que funcione

--- src/components/Sections/FormRegistro.jsx
+++ src/components/Sections/FormRegistro.jsx
@@ -2,233 +2,216 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Button, FormGroup, FormFeedback, Input, Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from 'next/link';
-
-function LlamarALaravel(typeUser) {
-  let endpointRegitrarUsuario = "/User/create/" + typeUser; // "professional" si es un prefesional, o "client" si es un cliente
-  //la U de User tiene que estar en mayúscula si o si
-
-  fetch(process.env.NEXT_PUBLIC_API_URL + endpointRegitrarUsuario, {
-    method: "post",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: "email@gmail.com", //éstos 4 puntos son obligatorios
-      name: "gonzalo corvalan",
-      firebaseId: "0iwjlksajlecgouh78900",
-      lineOfWork: 10, // si es 1 es un cliente, 2 el rubro en el que trabaja no está en la lista, entre 3 y 44 es la lista de rubros actual
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-      return false;
-    });
-}
+import Link from "next/link";
 
 export default function FormRegistro({ onSubmit }) {
-  const { register, handleSubmit, errors, getValues } = useForm();
+    const { register, handleSubmit, errors, getValues } = useForm();
 
-  return (
-    <React.Fragment>
-      <form noValidate onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup className="row">
-          <label
-            htmlFor="name"
-            className="col-md-4 col-form-label text-md-right"
-          >
-            Nombre Completo:
-          </label>
-          <Col md="8">
-            <Input
-              id="name"
-              placeholder="Nombre"
-              name="name"
-              invalid={"name" in errors}
-              valid={!errors.name && getValues("name") && true}
-              innerRef={register({
-                required: "Escribí tu nombre",
-              })}
-            />
-            <FormFeedback>{errors?.name?.message}</FormFeedback>
-          </Col>
-        </FormGroup>
-        <FormGroup className="row">
-          <label
-            htmlFor="email"
-            className="col-md-4 col-form-label text-md-right"
-          >
-            Email:
-          </label>
-          <Col md="8">
-            <Input
-              id="email"
-              placeholder="Email"
-              type="email"
-              name="email"
-              invalid={"email" in errors}
-              valid={!errors.email && getValues("email") && true}
-              innerRef={register({
-                required: "Escribí tu email",
-                pattern: {
-                  value: /^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  message: "Escribí un email válido",
-                },
-              })}
-            />
-            <FormFeedback>{errors?.email?.message}</FormFeedback>
-          </Col>
-        </FormGroup>
-        <FormGroup className="row">
-          <label
-            htmlFor="password"
-            className="col-md-4 col-form-label text-md-right"
-          >
-            Contraseña y Confirmación:
-          </label>
-          <Col md="4" className="pb-3">
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Contraseña"
-              invalid={"password" in errors}
-              valid={!errors.password && getValues("password") && true}
-              innerRef={register({
-                required: "Escribí una contraseña",
-                validate: (value) =>
-                  value.length >= 6 || 'La contraseña tiene que tenér al menos 6 carácteres'
-              })}
-            />
-            <FormFeedback>{errors?.password?.message}</FormFeedback>
-          </Col>
-          <Col md="4">
-            <Input
-              name="rePassword"
-              type="password"
-              placeholder="Repetir contraseña"
-              invalid={"rePassword" in errors}
-              valid={
-                !errors.rePassword && getValues("rePassword") && true
-              }
-              innerRef={register({
-                required: "Repetí la contraseña",
-                validate: (value) =>
-                  value === getValues("password") ||
-                  "Las contraseñas tienen que ser iguales",
-              })}
-            />
-            <FormFeedback>
-              {errors?.rePassword?.message}
-            </FormFeedback>
-          </Col>
-        </FormGroup>
-        <FormGroup className="row">
-          <label
-            htmlFor="name"
-            className="col-md-4 col-form-label text-md-right"
-          >
-            En qué categoría estás?
+    return (
+        <React.Fragment>
+            <form noValidate onSubmit={handleSubmit(onSubmit)}>
+                <FormGroup className="row">
+                    <label
+                        htmlFor="name"
+                        className="col-md-4 col-form-label text-md-right"
+                    >
+                        Nombre Completo:
                     </label>
-          <Col md="4">
-            <div className="custom-control custom-radio my-3">
-              <input
-                className="custom-control-input"
-                id="client"
-                name="userType"
-                type="radio"
-                defaultChecked
-                value="client"
-                ref={register({ required: true })}
-              />
-              <label
-                className="custom-control-label"
-                htmlFor="client"
-              >
-                Soy Cliente
-              </label>
-            </div>
-          </Col>
-          <Col md="4">
-            <div className="custom-control custom-radio my-3">
-              <input
-                className="custom-control-input"
-                id="professional"
-                name="userType"
-                type="radio"
-                value="professional"
-                ref={register({ required: true })}
-              />
-              <label
-                className="custom-control-label"
-                htmlFor="professional"
-              >
-                Soy Profesional
-               </label>
-            </div>
-          </Col>
-        </FormGroup>
-        <Row className="justify-content-center">
-          <Button type="submit" color="success">
-            registráte ahora
-          </Button>
-        </Row>
-      </form>
-      <Row className="align-items-center">
-        <Col xs="3" md="4">
-          <hr className="w-100"></hr>
-        </Col>
-        <Col>
-          <p className="text-center my-auto">
-            O también con
-          </p>
-        </Col>
-        <Col xs="3" md="4">
-          <hr className="w-100"></hr>
-        </Col>
-      </Row>
-      <Row>
-        <Col md="6">
-          <Button block color="facebook" size="lg" onClick={() => onSubmit({ type: 'FB_SIGN_IN' })}>
-            <span className="btn-inner--icon mr-2">
-              <FontAwesomeIcon
-                icon={["fab", "facebook"]}
-                size="1x"
-              />
-            </span>
-            <span className="btn-inner--text">Facebook</span>
-          </Button>
-        </Col>
-        <Col md="6">
-          <Button
-            block
-            onClick={() => onSubmit({ type: 'GOOGLE_SIGN_IN' })}
-            className="text-white mt-4 mt-md-0"
-            size="lg"
-            style={{
-              background: "#4285f4",
-            }}
-          >
-            <span className="btn-inner--icon mr-2">
-              <FontAwesomeIcon
-                icon={["fab", "google"]}
-                size="1x"
-              />
-            </span>
-            <span className="btn-inner--text">Google</span>
-          </Button>
-        </Col>
-      </Row>
-      <Row className="mt-2 justify-content-center">
-        <Link href="/ingresar">
-          <a>Ya tenés cuenta? Ingresá</a>
-        </Link>
-      </Row>
-    </React.Fragment >
-  );
+                    <Col md="8">
+                        <Input
+                            id="name"
+                            placeholder="Nombre"
+                            name="name"
+                            invalid={"name" in errors}
+                            valid={!errors.name && getValues("name") && true}
+                            innerRef={register({
+                                required: "Escribí tu nombre",
+                            })}
+                        />
+                        <FormFeedback>{errors?.name?.message}</FormFeedback>
+                    </Col>
+                </FormGroup>
+                <FormGroup className="row">
+                    <label
+                        htmlFor="email"
+                        className="col-md-4 col-form-label text-md-right"
+                    >
+                        Email:
+                    </label>
+                    <Col md="8">
+                        <Input
+                            id="email"
+                            placeholder="Email"
+                            type="email"
+                            name="email"
+                            invalid={"email" in errors}
+                            valid={!errors.email && getValues("email") && true}
+                            innerRef={register({
+                                required: "Escribí tu email",
+                                pattern: {
+                                    value: /^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                    message: "Escribí un email válido",
+                                },
+                            })}
+                        />
+                        <FormFeedback>{errors?.email?.message}</FormFeedback>
+                    </Col>
+                </FormGroup>
+                <FormGroup className="row">
+                    <label
+                        htmlFor="password"
+                        className="col-md-4 col-form-label text-md-right"
+                    >
+                        Contraseña y Confirmación:
+                    </label>
+                    <Col md="4" className="pb-3">
+                        <Input
+                            id="password"
+                            name="password"
+                            type="password"
+                            placeholder="Contraseña"
+                            invalid={"password" in errors}
+                            valid={
+                                !errors.password &&
+                                getValues("password") &&
+                                true
+                            }
+                            innerRef={register({
+                                required: "Escribí una contraseña",
+                                validate: (value) =>
+                                    value.length >= 6 ||
+                                    "La contraseña tiene que tenér al menos 6 carácteres",
+                            })}
+                        />
+                        <FormFeedback>{errors?.password?.message}</FormFeedback>
+                    </Col>
+                    <Col md="4">
+                        <Input
+                            name="rePassword"
+                            type="password"
+                            placeholder="Repetir contraseña"
+                            invalid={"rePassword" in errors}
+                            valid={
+                                !errors.rePassword &&
+                                getValues("rePassword") &&
+                                true
+                            }
+                            innerRef={register({
+                                required: "Repetí la contraseña",
+                                validate: (value) =>
+                                    value === getValues("password") ||
+                                    "Las contraseñas tienen que ser iguales",
+                            })}
+                        />
+                        <FormFeedback>
+                            {errors?.rePassword?.message}
+                        </FormFeedback>
+                    </Col>
+                </FormGroup>
+                <FormGroup className="row">
+                    <label
+                        htmlFor="name"
+                        className="col-md-4 col-form-label text-md-right"
+                    >
+                        En qué categoría estás?
+                    </label>
+                    <Col md="4">
+                        <div className="custom-control custom-radio my-3">
+                            <input
+                                className="custom-control-input"
+                                id="client"
+                                name="userType"
+                                type="radio"
+                                defaultChecked
+                                value="client"
+                                ref={register({ required: true })}
+                            />
+                            <label
+                                className="custom-control-label"
+                                htmlFor="client"
+                            >
+                                Soy Cliente
+                            </label>
+                        </div>
+                    </Col>
+                    <Col md="4">
+                        <div className="custom-control custom-radio my-3">
+                            <input
+                                className="custom-control-input"
+                                id="professional"
+                                name="userType"
+                                type="radio"
+                                value="professional"
+                                ref={register({ required: true })}
+                            />
+                            <label
+                                className="custom-control-label"
+                                htmlFor="professional"
+                            >
+                                Soy Profesional
+                            </label>
+                        </div>
+                    </Col>
+                </FormGroup>
+                <Row className="justify-content-center">
+                    <Button type="submit" color="success">
+                        registráte ahora
+                    </Button>
+                </Row>
+            </form>
+            <Row className="align-items-center">
+                <Col xs="3" md="4">
+                    <hr className="w-100"></hr>
+                </Col>
+                <Col>
+                    <p className="text-center my-auto">O también con</p>
+                </Col>
+                <Col xs="3" md="4">
+                    <hr className="w-100"></hr>
+                </Col>
+            </Row>
+            <Row>
+                <Col md="6">
+                    <Button
+                        block
+                        color="facebook"
+                        size="lg"
+                        onClick={() => onSubmit({ type: "FB_SIGN_IN" })}
+                    >
+                        <span className="btn-inner--icon mr-2">
+                            <FontAwesomeIcon
+                                icon={["fab", "facebook"]}
+                                size="1x"
+                            />
+                        </span>
+                        <span className="btn-inner--text">Facebook</span>
+                    </Button>
+                </Col>
+                <Col md="6">
+                    <Button
+                        block
+                        onClick={() => onSubmit({ type: "GOOGLE_SIGN_IN" })}
+                        className="text-white mt-4 mt-md-0"
+                        size="lg"
+                        style={{
+                            background: "#4285f4",
+                        }}
+                    >
+                        <span className="btn-inner--icon mr-2">
+                            <FontAwesomeIcon
+                                icon={["fab", "google"]}
+                                size="1x"
+                            />
+                        </span>
+                        <span className="btn-inner--text">Google</span>
+                    </Button>
+                </Col>
+            </Row>
+            <Row className="mt-2 justify-content-center">
+                <Link href="/ingresar">
+                    <a>Ya tenés cuenta? Ingresá</a>
+                </Link>
+            </Row>
+        </React.Fragment>
+    );
 }
--- src/pages/registrarse.jsx
+++ src/pages/registrarse.jsx
@@ -4,53 +4,58 @@ import { createAccount } from "../firebase";
 import FormRegistro from "../components/Sections/FormRegistro";
 
 export default function Registrarse() {
-  const onSubmit = (data) => {
-    createAccount(data.type, data.email, data.password, data.name).then(user => {
-      let endpointRegitrarUsuario = "/User/create/" + data.userType; // "professional" si es un prefesional, o "client" si es un cliente
-      //la U de User tiene que estar en mayúscula si o si
-      fetch(process.env.NEXT_PUBLIC_API_URL + endpointRegitrarUsuario, {
-        method: "post",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: user.email, //éstos 4 puntos son obligatorios
-          name: data.displayName || user.displayName,
-          firebaseId: user.id,
-          lineOfWork: 10, // si es 1 es un cliente, 2 el rubro en el que trabaja no está en la lista, entre 3 y 44 es la lista de rubros actual
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    })
-  };
+    const onSubmit = (data) => {
+        createAccount(data.type, data.email, data.password, data.name).then(
+            (user) => {
+                let endpointRegitrarUsuario = "/User/create/" + data.userType; // "professional" si es un prefesional, o "client" si es un cliente
+                //la U de User tiene que estar en mayúscula si o si
+                fetch(
+                    process.env.NEXT_PUBLIC_API_URL + endpointRegitrarUsuario,
+                    {
+                        method: "post",
+                        mode: "cors",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            email: user.email, //éstos 4 puntos son obligatorios
+                            name: data.displayName || user.displayName,
+                            firebaseId: user.id,
+                            lineOfWork: 1, // si es 1 es un cliente, 2 el rubro en el que trabaja no está en la lista, entre 3 y 44 es la lista de rubros actual
+                        }),
+                    }
+                )
+                    .then((res) => res.json())
+                    .then((data) => {
+                        console.log(data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        );
+    };
 
-  return (
-    <Layout title="ReparoAltoque - Registrarse">
-      <div className="main bg-gradient-warning">
-        <Container className="pt-8" style={{ minHeight: "100vh" }}>
-          <Row className="justify-content-center">
-            <Col md="10">
-              <Card>
-                <CardHeader>
-                  <h4 className="title text-primary display-4 mb-0">
-                    Registráte en 5 minutos
-                  </h4>
-                </CardHeader>
-                <CardBody>
-                  <FormRegistro onSubmit={onSubmit} />
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout title="ReparoAltoque - Registrarse">
+            <div className="main bg-gradient-warning">
+                <Container className="pt-8" style={{ minHeight: "100vh" }}>
+                    <Row className="justify-content-center">
+                        <Col md="10">
+                            <Card>
+                                <CardHeader>
+                                    <h4 className="title display-4 mb-0">
+                                        Registráte en 5 minutos
+                                    </h4>
+                                </CardHeader>
+                                <CardBody>
+                                    <FormRegistro onSubmit={onSubmit} />
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </Layout>
+    );
 }
